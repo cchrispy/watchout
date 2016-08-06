e741f21db7d38f3407fcd8980ef0bedd1c5b0b9a
cchrispy <cchiang1992@gmail.com>
2016-08-06 12:10:45 -0700

    Bare minimums complete
    Enemies now constant size; enemies are trump; enemies rotate (css)
    Highscores, scores, collision counter are functional

--- client/watchout.js
+++ client/watchout.js
@@ -8,219 +8,79 @@
 6. Use css3 animations to make the enemies whirling shuriken.
 */
 
-// gameRules = {
-//   height: 800,
-//   width: 800,
-//   numOfEnemies: 4
-// };
-
-// //refactor later
-// var enemyData;
-// var cursorCoordinates;
-// var ballx;
-// var bally;
-
-
-// var Enemy = function(id) {
-//   this.x = Math.random() * gameRules.height;
-//   this.y = Math.random() * gameRules.width;
-//   //remove if necessary
-//   this.id = id;
-// };
-
-// var createEnemyData = function() {
-//   var enemies = [];
-//   for (var i = 0; i < gameRules.numOfEnemies; i++) {
-//     enemies.push(new Enemy(i));
-//   }
-
-//   return enemies;
-// };
-
-// var drawEnemies = function() {
-//   enemyData = createEnemyData();
-//   d3.select('svg').selectAll('circle').data(enemyData)
-//     .enter().append('circle')
-//     .attr('fill', 'white')
-//     .attr('class', 'enemy')
-//     .attr('cx', function(d) {
-//       return d.x;
-//     })
-//     .attr('cy', function(d) {
-//       return d.y;
-//     })
-//     .attr('r', '100px')
-//     /*
-//     .on('mouseenter', function() {
-//       //reset
-//       //high score
-//       console.log('collision');
-//     })
-//     */
-//   ;
-// };
-
-// var updateEnemyPositions = function() {
-//   enemyData = createEnemyData();
-//   // console.log(enemyData);
-//   //select all enemies
-//   d3.select('svg').selectAll('circle').data(enemyData)
-//   //apply a transition so they move
-//     .transition().duration(1000)
-//   //update attributes cx and cy
-//     .attr('cx', function(d) {
-//       // check if x and y are touching the mouse
-//       // then reset score
-//       return d.x;
-//     })
-//     .attr('cy', function(d) {
-//       return d.y;
-//     });
-// };
-
-// var makeBall = function() {
-//   var ball = d3.select('svg').selectAll('circle').data([{id: 'ball'}], function(d) {
-//     return d.id;
-//   });
-
-//   //ball position update and collision detection
-//   var drag = d3.behavior.drag()
-//     .on('drag', function() {
-//       ball.attr('cx', cursorCoordinates[0]);
-//       ball.attr('cy', cursorCoordinates[1]);
-
-//       //apply collision detection when dragging
-//       d3.select('svg').selectAll('.enemy')
-//       .on('mouseenter', function() {
-//         console.log('collision');
-//       });
-//       //when let go of drag, overwrite mouseenter event listener to stop collision detection
-//       d3.select('svg').on('mouseup', function() {
-//         d3.select('svg').selectAll('.enemy')
-//         .on('mouseenter', null);
-//         ballx = d3.select('.ball').attr('cx');
-//         bally = d3.select('.ball').attr('cy');
-//       });
-//     });
-
-//   ball.enter().append('circle')
-//     .attr('fill', 'green')
-//     .attr('class', 'ball')
-//     .attr('cx', 100)
-//     .attr('cy', 100)
-//     .attr('r', '10px')
-//     .call(drag)
-//   ;
-// };
-
-// var updateCursorPosition = function() {
-//   svg.on('mousemove', function() {
-//     cursorCoordinates = d3.mouse(this);
-//   });
-// };
-
-// var currentScore = function() {
-//   var score = 0;
-  
-//   setInterval(function() {
-//     score++;
-//     d3.select('.current > span').data(score).text(score);
-//   }, 100);
-// };
-
-// var svg = d3.select('.board').append('svg')
-//                   .attr('height', gameRules.height)
-//                   .attr('width', gameRules.width);
-
-// drawEnemies();
-// setInterval(updateEnemyPositions, 1000);
-// currentScore();
-// updateCursorPosition();
-// makeBall();
-
-
-
-// on the board make enemies
-// data should be their {id: number, x: x, y: y}
-
-
-//determine the number of enemies
-
-//create all the enemies using constructor, put in dataset
-
-
-
-
-// svg.selectAll(enemies).data(dataStuff).append()
-// d3.selectAll('div').data([1, 2, 3]).enter().append()
-
-// update enemies by changing x and y based on the data
-
-
-
-// <circle cx="x" cy="y" fill="black" r="20">
-
-
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////
-
-
 gameBoard = {
   width: 600,
   height: 600,
-  enemies: 10
-}
+  enemies: 8,
+  score: 0,
+  collisions: 0
+};
 
 var svg = d3.select('.board').append('svg')
   .attr('width', gameBoard.width)
   .attr('height', gameBoard.height);
 
+var image = svg.append('defs').append('svg:pattern')
+  .attr('id', 'trump')
+  .attr('width', 1)
+  .attr('height', 1)
+  .append('svg:image')
+  .attr('width', 80)
+  .attr('height', 80)
+  .attr('xlink:href', 'trump.png');
+
+
 // svg.selectAll('circle').data(_.range(1, gameBoard.enemies))
-svg.selectAll('circle').data([1,2,3,4,5,6,7,8,9])
+svg.selectAll('circle').data(_.range(1, gameBoard.enemies))
   .enter()
+  // .append('svg:image')
+  // .attr('xlink:href', 'https://40.media.tumblr.com/e8258a36ccf95080f68f2ecf1a0ad021/tumblr_inline_nvs0vwJyjO1r33elg_540.png')
+  // .attr('height', 20)
+  // .attr('width', 20)
   .append('circle')
-  .attr('fill', 'black')
-  .attr('r', 10)
+  .style('fill', 'url(#trump)')
+  // .attr('r', function(d) {
+  //   return 15 + (Math.random() * 20);
+  // })
+  .attr('r', function(d) {
+    return 40;
+  })
   .attr('class', 'enemy')
   .attr('cx', function(d) {
-    return 10 + Math.random() * (gameBoard.width-20);
+    return 10 + Math.random() * (gameBoard.width - 20);
   })
   .attr('cy', function(d) {
-    return 10 + Math.random() * (gameBoard.height-20);
+    return 10 + Math.random() * (gameBoard.height - 20);
   });
 
 var drag = d3.behavior.drag()
 // drag behavior function adds listeners to objects that .call() it
             .on('dragstart', function() {
-              console.log('dragstart');
+
             })
             .on('drag', function() {
 // drag creates d3.event object, with x&y position of mouse              
               d3.select('.player').attr('cx', function() {
                 if (d3.event.x < 10) {
-                  return 10
-                } else if (d3.event.x > gameBoard.width-10) {
-                  return gameBoard.width-10;
+                  return 10;
+                } else if (d3.event.x > gameBoard.width - 10) {
+                  return gameBoard.width - 10;
                 } else { return d3.event.x; }
               });
               d3.select('.player').attr('cy', function() {
                 if (d3.event.y < 10) {
                   return 10;
-                } else if (d3.event.y > gameBoard.height-10) {
+                } else if (d3.event.y > gameBoard.height - 10) {
                   return gameBoard.height - 10;
                 } else { return d3.event.y; }
               });
-              console.log('dragging');
             })
             .on('dragend', function() {
-              console.log('dragend');
-            })
+
+            });
 
 var player = svg.selectAll('circle').data([{player: 'green'}],
-    function(d) { return d.player })
+    function(d) { return d.player; })
   .enter()
   .append('circle')
   .attr('class', 'player')
@@ -228,33 +88,72 @@ var player = svg.selectAll('circle').data([{player: 'green'}],
     return d.player;
   })
   .attr('r', 10)
-  .attr('cx', gameBoard.width/2)
-  .attr('cy', gameBoard.height/2)
+  .attr('cx', gameBoard.width / 2)
+  .attr('cy', gameBoard.height / 2)
   .call(drag); // drag event listeners added
 
 function update() {
   svg.selectAll('circle.enemy')
     .transition().duration(1000)
     .attr('cx', function(d) {
-      return 10 + Math.random() * (gameBoard.width-20);
+      return 10 + Math.random() * (gameBoard.width - 20);
     })
     .attr('cy', function(d) {
-      return 10 + Math.random() * (gameBoard.width-20);
+      return 10 + Math.random() * (gameBoard.width - 20);
     });
 }
+
+// we have a score that keeps increasing with time
+// the score gets reset to 0 upon a collision
+// the '.current > span' will always equal score
 
 function collisionCheck() {
   var enemy = svg.selectAll('.enemy');
   var playerX = svg.select('.player').attr('cx');
   var playerY = svg.select('.player').attr('cy');
   enemy[0].forEach(function(circle) {
-    if (Math.abs(circle.cx.animVal.value - playerX) < 10 && Math.abs(circle.cy.animVal.value - playerY) < 10) {
-      console.log('collision detected');
+    if (Math.abs(circle.cx.animVal.value - playerX) < circle.r.animVal.value + 10 && Math.abs(circle.cy.animVal.value - playerY) < circle.r.animVal.value + 10) {
+      gameBoard.score = 0;
+      svg.transition().duration(10)
+        .style('background-color', 'red')
+        .transition().duration(250)
+        .style('background-color', 'gold');
+      throttledCollisions();
     }
-  })
+  });
+
+  d3.select('.collisions').select('span').data([gameBoard.collisions]).text(function(d) {
+    return d;
+  });
 }
 
+var throttledCollisions = _.throttle(function() {
+  gameBoard.collisions++;
+}, 1200);
+
+var incrementScore = function(score) {
+  d3.select('.current').select('span').data([score]).text(function(d) {
+    return d;
+  });
+
+  var highscore = d3.select('.highscore').select('span');
+  highscore.data([score]).text(function(d) {
+    return Math.max(d, highscore.text());
+  });
+};
+
+// need to register collison counter, score, high score
+
 // collisionCheck();
-setInterval(collisionCheck, 50);
+// setInterval(collisionCheck, 10);
+setInterval(function() {
+  collisionCheck();
+  gameBoard.score++;
+  incrementScore(gameBoard.score);
+}, 10);
 // update();
 setInterval(update, 1000);
+
+
+
+
