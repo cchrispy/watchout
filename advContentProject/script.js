42f7e63182ccb8837634ecdb13f26885aa3e8fed
cchrispy <cchiang1992@gmail.com>
2016-08-06 17:00:12 -0700

    Started new data visualization project
    Average prison daily inmate population per year

---
+++ advContentProject/script.js
@@ -0,0 +1,56 @@
+var url = 'https://data.cityofnewyork.us/api/views/26ze-s5bx/rows.json';
+var data = [];
+var newData = [];
+
+var graph = d3.select('#graph').append('svg')
+  .attr('height', 700).attr('width', 700);
+
+var xScale = d3.scale.linear().range([60, 700 - 60]).domain([2000, 2010]);
+var yScale = d3.scale.linear().range([700 - 60, 60]).domain([12000, 16000]);
+var xAxis = d3.svg.axis().scale(xScale);
+var yAxis = d3.svg.axis().scale(yScale).orient('left');
+
+graph.append('svg:g').attr('transform', 'translate(0,' + (700 - 60) + ')').call(xAxis);
+graph.append('svg:g').attr('transform', 'translate(' + (60) + ', 0)').call(yAxis);
+
+
+var testing = function(something, json) {
+  //create array for line
+  json.data.forEach(function(item) {
+    data.push({
+      year: +item[8],
+      population: +item[9]
+    });
+    newData.push({
+      year: +item[8],
+      population: +item[9] - 200
+    });
+  });
+
+  var line = d3.svg.line()
+    .x(function(d) {
+      return xScale(d.year);
+    })
+    .y(function(d) {
+      return yScale(d.population);
+    })
+    .interpolate('basis');
+  
+  graph.append('svg:path').attr('d', line(data))
+    .attr('stroke', 'red')
+    .attr('stroke-width', 2)
+    .attr('fill', 'none');     
+
+  graph.append('svg:path').attr('d', line(newData))
+    .attr('stroke', 'blue')
+    .attr('stroke-width', 2)
+    .attr('fill', 'none');     
+};
+
+d3.json('https://data.cityofnewyork.us/api/views/26ze-s5bx/rows.json', function(json) {
+  //data = json.data;
+})
+    .mimeType('application/json')
+    .response(function(xhr) { return JSON.parse(xhr.responseText); })
+    .get(testing);
+
